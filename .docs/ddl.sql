49e3aca755ebaeb52cf8c803d4b873cc35d90070
Vivek Pillai <vpillai@ctisinc.com>
2019-06-04 16:35:55 -0400

    FPD Summary Scripts...

--- .docs/ddl.sql
+++ .docs/ddl.sql
@@ -1,3 +1,5 @@
+create extension if not exists "uuid-ossp";
+
 CREATE SEQUENCE public."FDPStaging_SummaryId_seq"
   INCREMENT 1
   MINVALUE 1
@@ -9,7 +11,7 @@ ALTER TABLE public."FDPStaging_SummaryId_seq" OWNER TO appuser;
   
 CREATE TABLE public."FPDStaging_Summary"
 (
-  "id" integer NOT NULL DEFAULT nextval('"FDPStaging_SummaryId_seq"'::regclass),
+  "id" uuid DEFAULT uuid_generate_v4 (),
   "reportDate" timestamp with time zone NOT NULL DEFAULT now(),
   "reportName" character varying(200) NOT NULL,
   "loadStatus" character varying(20) NOT NULL DEFAULT 'Started'::character varying,
@@ -22,11 +24,65 @@ ALTER TABLE public."FPDStaging_Summary" OWNER TO appuser;
 
 CREATE TABLE public."FPDStaging_AwardDetails"
 (
-  "reportDate" timestamp with time zone NOT NULL DEFAULT now(),
-  "reportName" character varying(200) NOT NULL,
-  "loadStatus" character varying(20) NOT NULL DEFAULT 'Started'::character varying,
-  "updatedOn" timestamp with time zone NOT NULL DEFAULT now(),
-  "FPDStaging_SummaryId" integer NOT NULL,
+  "id" uuid DEFAULT uuid_generate_v4(),
+  "FPDStaging_SummaryId" uuid NOT NULL,
+  "ContractingAgencyID" character varying(50), 
+  "ContractingAgencyName" character varying(50), 
+  "ContractingOfficeID" character varying(50), 
+  "ContractingOfficeName" character varying(50), 
+  "CountryAwardIssued" character varying(50), 
+  "FundingAgencyID" character varying(50), 
+  "FundingAgencyName" character varying(50), 
+  "FundingDepartmentName" character varying(50), 
+  "FundingOfficeID" character varying(50), 
+  "FundingOfficeName" character varying(50), 
+  "IsFundedForeign" character varying(50), 
+  "InherentlyGovFunction" character varying(50), 
+  "InherentlyGovDescription" character varying(50), 
+  "IDVDepartmentID" character varying(50), 
+  "IDVDepartmentName" character varying(50), 
+  "ReferencedIDVAgencyID" character varying(50), 
+  "ProgramAcronym" character varying(50), 
+  "IDVProgramAcronym" character varying(50), 
+  "PIID" character varying(50), 
+  "ReferencedIDVPIID" character varying(50), 
+  "SolicitationID" character varying(50), 
+  "FiscalYear" character varying(50), 
+  "DateSigned" character varying(50), 
+  "EffectiveDate" character varying(50), 
+  "CompletionDate" character varying(50), 
+  "EstUltimateCompletionDate" character varying(50), 
+  "LastDateToOrder" character varying(50), 
+  "TypeofContract" character varying(50), 
+  "TypeofSetAside" character varying(50), 
+  "NAICSCode" character varying(50), 
+  "NAICSDescription" character varying(50), 
+  "POPCity" character varying(50), 
+  "POPState" character varying(50), 
+  "ExtentCompeted" character varying(50), 
+  "AwardorIDVType" character varying(50), 
+  "SBIRSTTR" character varying(50), 
+  "VendorName" character varying(50), 
+  "DUNSNumber" character varying(50), 
+  "Zip" character varying(50), 
+  "WebsiteURL" character varying(50), 
+  "EmailAddress" character varying(50), 
+  "NumberofOffersReceived" character varying(50), 
+  "ModificationNumber" character varying(50), 
+  "ReasonForMod" character varying(50), 
+  "TransactionNumber" character varying(50), 
+  "OTFAC" character varying(50), 
+  "PBC" character varying(50), 
+  "SolicitationProcedures" character varying(50), 
+  "CostPricingData" character varying(50), 
+  "A76ActionCode" character varying(50), 
+  "TypeofIDC" character varying(50), 
+  "PreAwardFBOSynopsis" character varying(50), 
+  "BaseAndAllOptionsValue" character varying(50), 
+  "TotalContractValue" character varying(50), 
+  "ActionObligation" character varying(50), 
+  "Actions" character varying(50), 
+   
   CONSTRAINT FPD_AwardsStaging_Loads_foreign FOREIGN KEY ("FPDStaging_SummaryId")
       REFERENCES public."FPDStaging_Summary" (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -38,9 +94,3 @@ ALTER TABLE public."FPDStaging_AwardDetails" OWNER TO appuser;
 DROP TABLE public."FPDStaging_AwardDetails";
 DROP TABLE public."FPDStaging_Summary";
 DROP SEQUENCE public."FDPStaging_SummaryId_seq";
-
-
-Contracting Agency ID, Contracting Agency Name, Contracting Office ID, Contracting Office Name, Country Where Award was Issued, Funding Agency ID, Funding Agency Name, Funding Department Name, Funding Office ID, Funding Office Name, Is Funded by Foreign Entity (Foreign Funding) Code, Inherently Governmental Description, Inherently Governmental Function, IDV Department ID, IDV Department Name, Referenced IDV Agency ID, (Program Acronym), IDV Program Acronym, PIID, Referenced  IDV PIID, Solicitation ID, Fiscal Year, Date Signed, Effective Date, Completion Date, Est. Ultimate Completion Date, (Last Date To Order), Type of Contract, Type of Set Aside, NAICS Code, NAICS Description, Principal Place of Performance City Name, Principal Place of Performance State Code, Extent Competed, Award or IDV Type, SBIR STTR, Vendor Name, DUNS Number, Zip, (Website URL), Email Address, Number of Offers Received, Modification Number, Reason For Modification Description, Transaction Number, Other Than Full and Open Competition, Performance Based Service Acquisition Code, Solicitation Procedures, Cost or Pricing Data, A-76 Action Code, (Type of IDC), Pre Award FBO Synopsis, Base and All Options Value (Total Contract Value), Action Obligation, Actions
-
-ContractingAgencyID, ContractingAgencyName, ContractingOfficeID, ContractingOfficeName, CountryAwardIssued, FundingAgencyID, FundingAgencyName, FundingDepartmentName, FundingOfficeID, FundingOfficeName, IsFundedForeign, InherentlyGovFunction, InherentlyGovDescription, IDVDepartmentID, IDVDepartmentName, ReferencedIDVAgencyID, ProgramAcronym, IDVProgramAcronym, PIID, ReferencedIDVPIID, SolicitationID, FiscalYear, DateSigned, EffectiveDate, CompletionDate, EstUltimateCompletionDate, LastDateToOrder, TypeofContract, TypeofSetAside, NAICSCode, NAICSDescription, POPCity, POPState, ExtentCompeted, AwardorIDVType, SBIRSTTR, VendorName, DUNSNumber, Zip, WebsiteURL, EmailAddress, NumberofOffersReceived, ModificationNumber, ReasonForMod, TransactionNumber, OTFAC, PBC, SolicitationProcedures, CostPricingData, A76ActionCode, TypeofIDC, PreAwardFBOSynopsis, BaseAndAllOptionsValue, TotalContractValue, ActionObligation, Actions
-
