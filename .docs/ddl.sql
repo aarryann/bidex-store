88ce769cdb40ebb4ba2b51ce85819e631915cb73
Vivek Pillai <vpillai@ctisinc.com>
2019-06-05 14:54:43 -0400

    FPD imports...

--- .docs/ddl.sql
+++ .docs/ddl.sql
@@ -1,51 +1,43 @@
 create extension if not exists "uuid-ossp";
 
-CREATE SEQUENCE public."FDPStaging_SummaryId_seq"
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 1
-  CACHE 1;
-
-ALTER TABLE public."FDPStaging_SummaryId_seq" OWNER TO appuser;
-  
-CREATE TABLE public."FPDStaging_Summary"
+CREATE TABLE public."Staging_Summary"
 (
   "id" uuid DEFAULT uuid_generate_v4 (),
   "reportDate" timestamp with time zone NOT NULL DEFAULT now(),
   "reportName" character varying(200) NOT NULL,
   "loadStatus" character varying(20) NOT NULL DEFAULT 'Started'::character varying,
   "updatedOn" timestamp with time zone NOT NULL DEFAULT now(),
-  CONSTRAINT "FPDStaging_Summary_pkey" PRIMARY KEY (id)
+  CONSTRAINT "Staging_Summary_pkey" PRIMARY KEY (id)
 )
 WITH ( OIDS=FALSE );
 
-ALTER TABLE public."FPDStaging_Summary" OWNER TO appuser;
+ALTER TABLE public."Staging_Summary" OWNER TO appuser;
 
 CREATE TABLE public."FPDStaging_AwardDetails"
 (
   "id" uuid DEFAULT uuid_generate_v4(),
-  "FPDStaging_SummaryId" uuid NOT NULL,
+  "Staging_SummaryId" uuid NOT NULL,
+  "PIID" character varying(50), 
+  "ReferencedIDVPIID" character varying(50), 
+  "IDVProgramAcronym" character varying(50), 
+  "ReqDescription" character varying(4000), 
   "ContractingAgencyID" character varying(50), 
-  "ContractingAgencyName" character varying(50), 
+  "ContractingAgencyName" character varying(200), 
   "ContractingOfficeID" character varying(50), 
-  "ContractingOfficeName" character varying(50), 
+  "ContractingOfficeName" character varying(200), 
   "CountryAwardIssued" character varying(50), 
   "FundingAgencyID" character varying(50), 
-  "FundingAgencyName" character varying(50), 
-  "FundingDepartmentName" character varying(50), 
+  "FundingAgencyName" character varying(200), 
+  "FundingDepartmentName" character varying(200), 
   "FundingOfficeID" character varying(50), 
-  "FundingOfficeName" character varying(50), 
+  "FundingOfficeName" character varying(200), 
   "IsFundedForeign" character varying(50), 
   "InherentlyGovFunction" character varying(50), 
   "InherentlyGovDescription" character varying(50), 
   "IDVDepartmentID" character varying(50), 
-  "IDVDepartmentName" character varying(50), 
+  "IDVDepartmentName" character varying(200), 
   "ReferencedIDVAgencyID" character varying(50), 
   "ProgramAcronym" character varying(50), 
-  "IDVProgramAcronym" character varying(50), 
-  "PIID" character varying(50), 
-  "ReferencedIDVPIID" character varying(50), 
   "SolicitationID" character varying(50), 
   "FiscalYear" character varying(50), 
   "DateSigned" character varying(50), 
@@ -53,19 +45,19 @@ CREATE TABLE public."FPDStaging_AwardDetails"
   "CompletionDate" character varying(50), 
   "EstUltimateCompletionDate" character varying(50), 
   "LastDateToOrder" character varying(50), 
-  "TypeofContract" character varying(50), 
-  "TypeofSetAside" character varying(50), 
+  "TypeofContract" character varying(100), 
+  "TypeofSetAside" character varying(100), 
   "NAICSCode" character varying(50), 
-  "NAICSDescription" character varying(50), 
+  "NAICSDescription" character varying(200), 
   "POPCity" character varying(50), 
   "POPState" character varying(50), 
-  "ExtentCompeted" character varying(50), 
+  "ExtentCompeted" character varying(100), 
   "AwardorIDVType" character varying(50), 
-  "SBIRSTTR" character varying(50), 
-  "VendorName" character varying(50), 
+  "SBIRSTTR" character varying(100), 
+  "VendorName" character varying(200), 
   "DUNSNumber" character varying(50), 
   "Zip" character varying(50), 
-  "WebsiteURL" character varying(50), 
+  "WebsiteURL" character varying(200), 
   "EmailAddress" character varying(50), 
   "NumberofOffersReceived" character varying(50), 
   "ModificationNumber" character varying(50), 
@@ -83,8 +75,8 @@ CREATE TABLE public."FPDStaging_AwardDetails"
   "ActionObligation" character varying(50), 
   "Actions" character varying(50), 
    
-  CONSTRAINT FPD_AwardsStaging_Loads_foreign FOREIGN KEY ("FPDStaging_SummaryId")
-      REFERENCES public."FPDStaging_Summary" (id) MATCH SIMPLE
+  CONSTRAINT FPD_AwardsStaging_Loads_foreign FOREIGN KEY ("Staging_SummaryId")
+      REFERENCES public."Staging_Summary" (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
@@ -92,5 +84,7 @@ ALTER TABLE public."FPDStaging_AwardDetails" OWNER TO appuser;
 
 
 DROP TABLE public."FPDStaging_AwardDetails";
-DROP TABLE public."FPDStaging_Summary";
-DROP SEQUENCE public."FDPStaging_SummaryId_seq";
+DROP TABLE public."Staging_Summary";
+
+SELECT pg_size_pretty( pg_total_relation_size('"Staging_Summary"') );
+SELECT pg_size_pretty( pg_total_relation_size('"FPDStaging_AwardDetails"') );
